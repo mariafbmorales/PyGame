9c0ee1a3ba314d7d50d1dcf2f377bda8200f4d89
Maria <nanda031006@gmail.com>
2025-05-12 10:58:04 -0300

    loop principal

--- jogo.py
+++ jogo.py
@@ -3,5 +3,14 @@ import pygame
 pygame.init() 
 
 #Gera tela principal
-window = pygame.display.set_mode((700, 500))
-pygame.display.set_caption('No 1 rodou')
+window = pygame.display.set_mode((600, 750))
+pygame.display.set_caption('No 1, rodou')
+
+#Inicia estruturade dados
+game = True
+
+#Loop principal
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
