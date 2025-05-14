c78d7c5dd2b83612526273f72df979f72be2f97b
Maria <nanda031006@gmail.com>
2025-05-14 09:26:14 -0300

    mudando imagens

--- jogo.py
+++ jogo.py
@@ -12,17 +12,17 @@ pygame.display.set_caption('No 1, rodou')
 #Inicia assets
 DADO_WIDTH = 70
 DADO_HEIGHT = 70
-dado1_img = pygame.image.load('assets/img/dado1.jpeg').convert_alpha()
+dado1_img = pygame.image.load('assets/img/dado1-removebg-preview.png').convert_alpha()
 dado1_img_small = pygame.transform.scale(dado1_img, (DADO_WIDTH, DADO_HEIGHT))
-dado2_img = pygame.image.load('assets/img/dado2.jpeg').convert_alpha()
+dado2_img = pygame.image.load('assets/img/dado2-removebg-preview.png').convert_alpha()
 dado2_img_small = pygame.transform.scale(dado2_img, (DADO_WIDTH, DADO_HEIGHT))
-dado3_img = pygame.image.load('assets/img/dado3.jpeg').convert_alpha()
+dado3_img = pygame.image.load('assets/img/dado3-removebg-preview.png').convert_alpha()
 dado3_img_small = pygame.transform.scale(dado3_img, (DADO_WIDTH, DADO_HEIGHT))
-dado4_img = pygame.image.load('assets/img/dado4.jpeg').convert_alpha()
+dado4_img = pygame.image.load('assets/img/dado4-removebg-preview.png').convert_alpha()
 dado4_img_small = pygame.transform.scale(dado4_img, (DADO_WIDTH, DADO_HEIGHT))
-dado5_img = pygame.image.load('assets/img/dado5.jpeg').convert_alpha()
+dado5_img = pygame.image.load('assets/img/dado5-removebg-preview.png').convert_alpha()
 dado5_img_small = pygame.transform.scale(dado5_img, (DADO_WIDTH, DADO_HEIGHT))
-dado6_img = pygame.image.load('assets/img/dado6.jpeg').convert_alpha()
+dado6_img = pygame.image.load('assets/img/dado6-removebg-preview.png').convert_alpha()
 dado6_img_small = pygame.transform.scale(dado6_img, (DADO_WIDTH, DADO_HEIGHT))
 lista_dados = [0,dado1_img_small, dado2_img_small, dado3_img_small, dado4_img_small, dado5_img_small, dado6_img_small]
 
